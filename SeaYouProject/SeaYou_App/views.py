c070aa8aacef7764764a9c45371a1ed2226a410f
cNathalie <nathalie.courteaux@student.hogent.be>
2024-01-20 14:15:22 +0100

    Flow for routes
    ships -> ship visits -> ship visit displayed on map

--- SeaYouProject/SeaYou_App/views.py
+++ SeaYouProject/SeaYou_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from SeaYou_Models.models import Waypoint, Ship, Route
 
@@ -60,6 +61,24 @@ def shipVisits(request, ship_imo):
 
 
     return render(request, 'ship-visits.html', context)
+
+def get_routes_for_visit(request, ship_imo, visit_id):
+    ship = Ship.objects.get(imo=ship_imo)
+    routes = list(Route.objects.filter(shipid=ship, visit=visit_id).values( 'visit', 'journey', 'waypointid',
+        'waypointid__waypointlatitude', 'waypointid__waypointlongitude', 'waypointid__waypointdescription', 'waypointdt', 'dockeddt', 'routecategoryid'))
+    
+    # Only one timestamp per waypoint needed
+    unique_waypoint_ids = set()
+    unique_routes = []
+
+    for route in routes:
+        waypoint_id = route['waypointid']
+        if waypoint_id not in unique_waypoint_ids:
+            unique_waypoint_ids.add(waypoint_id)
+            unique_routes.append(route)
+    
+
+    return JsonResponse(unique_routes, safe=False)
 
 def weather(request):
     return render(request, 'weather.html')
---
+++ SeaYouProject/static/SeaYou_Scripts/VisitRouteMapScript.js
@@ -0,0 +1,170 @@
+/* 
+Javascript to iterate over the ship's visit's route-waypoints,
+put them on the map and add a polyline between them.
+Using SnakeAnim plugin
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  let markerList = document.getElementById("markerList");
+  let chosenVisit = document.getElementById("chosenVisit");
+  let group = [];
+  let map = L.map("mapVisits").setView([51.379, 3.6072], 9);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  // Event listener for click on visit links
+  document.querySelectorAll(".visit-link").forEach((link) => {
+    link.addEventListener("click", async (event) => {
+      event.preventDefault();
+      const visitId = link.dataset.visitId;
+      const shipImo = link.dataset.shipImo;
+      group = [];
+      await clearMap();
+      //await clearMarkerList();
+      await setChosenVisit(visitId);
+      fetchRoutesForVisit(visitId, shipImo);
+    });
+  });
+
+  const fetchRoutesForVisit = (visitId, shipImo) => {
+    fetch(`/api/get_routes_for_visit/${shipImo}/${visitId}/`)
+      .then((response) => response.json())
+      .then((routes) => {
+        updateMapWithRoutes(routes);
+      })
+      .catch((error) => {
+        console.error("Error fetching routes" + error);
+      });
+  };
+
+  const updateMapWithRoutes = (routes) => {
+    console.log(routes);
+    for (let i = 0; i < routes.length; i++) {
+      const lat = parseFloat(routes[i].waypointid__waypointlatitude);
+      const lng = parseFloat(routes[i].waypointid__waypointlongitude);
+      const description = routes[i].waypointid__waypointdescription;
+      const datetime = routes[i].waypointdt
+        ? new Date(routes[i].waypointdt).toLocaleString("en-US")
+        : "unknown";
+      const dockeddt = routes[i].dockeddt
+        ? new Date(routes[i].dockeddt).toLocaleString("en-US")
+        : "unknown";
+      const popupText = `<strong>${description}</strong> <br/> <strong>Pinged at:</strong> ${datetime}`;
+
+      if (
+        typeof lat === "number" &&
+        typeof lng === "number" &&
+        !isNaN(lat) &&
+        !isNaN(lng)
+      ) {
+        const marker = L.marker([lat, lng]);
+        marker.bindPopup(popupText).on("add", () => {
+          marker.openPopup();
+        });
+
+        group.push(marker);
+
+        if (i < routes.length - 1) {
+          const nextRoute = routes[i + 1];
+          if (nextRoute) {
+            const nextLat = parseFloat(nextRoute.waypointid__waypointlatitude);
+            const nextLng = parseFloat(nextRoute.waypointid__waypointlongitude);
+            const polyline = L.polyline([
+              [lat, lng],
+              [nextLat, nextLng],
+            ]);
+
+            group.push(polyline);
+          }
+        } else {
+          // Adding Port of Antwerp as last stop with docked time
+          const fixedPointLat = 51.310207;
+          const fixedPointLng = 4.275428;
+          const lastPolyline = L.polyline([
+            [lat, lng],
+            [fixedPointLat, fixedPointLng],
+          ]);
+          group.push(lastPolyline);
+
+          const lastMarker = L.marker([fixedPointLat, fixedPointLng])
+            .bindPopup(
+              `<strong>Port of Antwerp</strong> <br/> <strong>Docked at:</strong> ${dockeddt}`
+            )
+            .on("add", () => {
+              lastMarker.openPopup();
+            });
+          group.push(lastMarker);
+        }
+      }
+    }
+
+    const route = L.featureGroup(group, { snakingPause: 800 });
+
+    map.fitBounds(route.getBounds());
+
+    map.addLayer(route);
+
+    const snake = () => {
+      route.snakeIn();
+    };
+
+    snake();
+  };
+
+  const addToMarkerList = async (marker) => {
+    const newMarker = document.createElement("p");
+    newMarker.classList.add("markerItem");
+    const node = document.createTextNode(marker);
+    newMarker.appendChild(node);
+    markerList.appendChild(newMarker);
+  };
+
+  const clearMarkerList = async () => {
+    const markerItems = Array.from(
+      document.getElementsByClassName("markerItem")
+    );
+    for (let index = 0; index < markerItems.length; index++) {
+      const element = markerItems[index];
+      element.remove();
+    }
+  };
+
+  const setChosenVisit = async (visitId) => {
+    while (chosenVisit.hasChildNodes()) {
+      chosenVisit.removeChild(chosenVisit.firstChild);
+    }
+    const textNode = document.createTextNode(`Visit: ${visitId}`);
+    chosenVisit.appendChild(textNode);
+  };
+
+  const clearMap = async () => {
+    map.eachLayer(function (layer) {
+      // Check if the layer is a Marker or Polyline
+      if (layer instanceof L.Marker || layer instanceof L.Polyline) {
+        map.removeLayer(layer);
+      }
+    });
+  };
+});
+
+const toggleTableBody = () => {
+  const button = document.getElementById("toggleTableBody");
+  const tableBody = document.getElementById("visitsTableBody");
+  tableBody.classList.toggle("hidden");
+  // Check the current content of the button
+  if (button.innerHTML === "↓") {
+    // If it's "↓", change it to "↑"
+    button.innerHTML = "↑";
+    // Add logic for when the arrow is pointing up
+    // ...
+  } else {
+    // If it's not "↓", change it back to "↓"
+    button.innerHTML = "↓";
+    // Add logic for when the arrow is pointing down
+    // ...
+  }
+}
