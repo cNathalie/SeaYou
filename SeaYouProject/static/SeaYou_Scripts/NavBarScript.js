b255818e1403df0923bbfd8c1fff6a1d820b8a6a
cNathalie <nathalie.courteaux@student.hogent.be>
2024-01-21 16:39:54 +0100

    CleanUp

---
+++ SeaYouProject/static/SeaYou_Scripts/NavBarScript.js
@@ -0,0 +1,9 @@
+
+  const navbar = document.getElementById('navbar'); 
+  const dropdown = document.getElementById('dropdownNavbar'); 
+  const dropdownTrigger = document.getElementById('dropdownNavbarLink');
+  dropdownTrigger.addEventListener('click', () =>{dropdown.classList.toggle("hidden")}); 
+  
+  const sandwich = document.getElementById('navbarSandwich'); 
+  sandwich.addEventListener('click',() => {navbar.classList.toggle("hidden")})
+
